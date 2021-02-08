c210b3499ac639dd45b81648964d2f6c6a533c7c
Dan Krasilnikov <krasilneg@yandex.ru>
2021-02-08 22:23:06 +1000

    v2.0.0

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  data: require('./lib/data'),
+  meta: require('./lib/meta'),
+  util: require('./lib/util'),
+  CollectionFieldModes: require('./lib/CollectionFieldModes'),
+  ConditionTypes: require('./lib/ConditionTypes'),
+  DateSizes: require('./lib/DateSizes'),
+  DateTypes: require('./lib/DateTypes'),
+  FieldModes: require('./lib/FieldModes'),
+  FieldSizes: require('./lib/FieldSizes'),
+  FieldTypes: require('./lib/FieldTypes'),
+  FunctionCodes: require('./lib/FunctionCodes'),
+  OperationTypes: require('./lib/OperationTypes'),
+  PropertyTypes: require('./lib/PropertyTypes'),
+  SortingModes: require('./lib/SortingModes'),
+  InsertPreprocessor: require('./lib/InsertPreprocessor'),
+  KeyProvider: require('./lib/KeyProvider'),
+  ListPreprocessor: require('./lib/ListPreprocessor'),
+  WorkflowProvider: require('./lib/WorkflowProvider')
+};
